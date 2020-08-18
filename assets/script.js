c46bde8d62eab93d76cab2dda319ae00fa36f143
Jordan Triplett <JordanT111596@gmail.com>
2020-08-18 11:14:51 -0400

    Added search button after results populate, added IDs

--- assets/script.js
+++ assets/script.js
@@ -103,110 +103,110 @@ count = 10; //placeholder value
 
 $('input.autocomplete').autocomplete({
   data: {
-      "African": null,
-      "American": null,
-      "Argentine": null,
-      "Armenian": null,
-      "Asian": null,
-      "BBQ": null,
-      "Bagels": null,
-      "Bakery": null,
-      "Bar Food": null,
-      "Beverages": null,
-      "Brazilian": null,
-      "Breakfast": null,
-      "British": null,
-      "Bubble Tea": null,
-      "Burger": null,
-      "Cafe": null,
-      "Cajun": null,
-      "California": null,
-      "Caribbean": null,
-      "Chinese": null,
-      "Coffee and Tea": null,
-      "Colombian": null,
-      "Crepes": null,
-      "Cuban": null,
-      "Deli": null,
-      "Desserts": null,
-      "Dim Sum": null,
-      "Diner": null,
-      "Dominican": null,
-      "Donuts": null,
-      "Drinks Only": null,
-      "Eastern European": null,
-      "Ethiopian": null,
-      "European": null,
-      "Fast Food": null,
-      "Floribbean": null,
-      "Fondue": null,
-      "French": null,
-      "Frozen Yogurt": null,
-      "Fusion": null,
-      "German": null,
-      "Greek": null,
-      "Grill": null,
-      "Hawaiian": null,
-      "Healthy Food": null,
-      "Ice Cream": null,
-      "Indian": null,
-      "International": null,
-      "Irish": null,
-      "Italian": null,
-      "Jamaican": null,
-      "Japanese": null,
-      "Juices": null,
-      "Kebab": null,
-      "Korean": null,
-      "Laotian": null,
-      "Latin American": null,
-      "Lebanese": null,
-      "Mediterranean": null,
-      "Mexican": null,
-      "Middle Eastern": null,
-      "Mongolian": null,
-      "Moroccan": null,
-      "Nepalese": null,
-      "New American": null,
-      "Pakistani": null,
-      "Patisserie": null,
-      "Peruvian": null,
-      "Pizza": null,
-      "Polish": null,
-      "Pub Food": null,
-      "Ramen": null,
-      "Russian": null,
-      "Salad": null,
-      "Salvadorean": null,
-      "Sandwich": null,
-      "Seafood": null,
-      "Soul Food": null,
-      "Southern": null,
-      "Southwestern": null,
-      "Spanish": null,
-      "Steak": null,
-      "Sushi": null,
-      "Swedish": null,
-      "Taco": null,
-      "Taiwanese": null,
-      "Tapas": null,
-      "Tea": null,
-      "Teriyaki": null,
-      "Tex-Mex": null,
-      "Thai": null,
-      "Turkish": null,
-      "Vegetarian": null,
-      "Vietnamese": null
+    "African": null,
+    "American": null,
+    "Argentine": null,
+    "Armenian": null,
+    "Asian": null,
+    "BBQ": null,
+    "Bagels": null,
+    "Bakery": null,
+    "Bar Food": null,
+    "Beverages": null,
+    "Brazilian": null,
+    "Breakfast": null,
+    "British": null,
+    "Bubble Tea": null,
+    "Burger": null,
+    "Cafe": null,
+    "Cajun": null,
+    "California": null,
+    "Caribbean": null,
+    "Chinese": null,
+    "Coffee and Tea": null,
+    "Colombian": null,
+    "Crepes": null,
+    "Cuban": null,
+    "Deli": null,
+    "Desserts": null,
+    "Dim Sum": null,
+    "Diner": null,
+    "Dominican": null,
+    "Donuts": null,
+    "Drinks Only": null,
+    "Eastern European": null,
+    "Ethiopian": null,
+    "European": null,
+    "Fast Food": null,
+    "Floribbean": null,
+    "Fondue": null,
+    "French": null,
+    "Frozen Yogurt": null,
+    "Fusion": null,
+    "German": null,
+    "Greek": null,
+    "Grill": null,
+    "Hawaiian": null,
+    "Healthy Food": null,
+    "Ice Cream": null,
+    "Indian": null,
+    "International": null,
+    "Irish": null,
+    "Italian": null,
+    "Jamaican": null,
+    "Japanese": null,
+    "Juices": null,
+    "Kebab": null,
+    "Korean": null,
+    "Laotian": null,
+    "Latin American": null,
+    "Lebanese": null,
+    "Mediterranean": null,
+    "Mexican": null,
+    "Middle Eastern": null,
+    "Mongolian": null,
+    "Moroccan": null,
+    "Nepalese": null,
+    "New American": null,
+    "Pakistani": null,
+    "Patisserie": null,
+    "Peruvian": null,
+    "Pizza": null,
+    "Polish": null,
+    "Pub Food": null,
+    "Ramen": null,
+    "Russian": null,
+    "Salad": null,
+    "Salvadorean": null,
+    "Sandwich": null,
+    "Seafood": null,
+    "Soul Food": null,
+    "Southern": null,
+    "Southwestern": null,
+    "Spanish": null,
+    "Steak": null,
+    "Sushi": null,
+    "Swedish": null,
+    "Taco": null,
+    "Taiwanese": null,
+    "Tapas": null,
+    "Tea": null,
+    "Teriyaki": null,
+    "Tex-Mex": null,
+    "Thai": null,
+    "Turkish": null,
+    "Vegetarian": null,
+    "Vietnamese": null
   }, limit: 3
 });
-    
-$('#userInput').on('submit', function() {
+
+$('#userInput').on('submit', function () {
   event.preventDefault();
-      // Make sure browser supports this feature
+  // Make sure browser supports this feature
   if (navigator.geolocation) {
     // Provide our showPosition() function to getCurrentPosition
     navigator.geolocation.getCurrentPosition(showPosition);
-  } 
+  }
   else {
     alert("Geolocation is not supported by this browser.");
   }
@@ -223,48 +223,55 @@ function showPosition(position) {
   zomatoCall(lat, lon);
 }
 
-function zomatoCall(lat,lon){
+function zomatoCall(lat, lon) {
   //inputCuisine = $('#cuisine').val();
   inputCuisine = $('#cuisine').val();
   cuisine = cuisineIds[inputCuisine];
-  console.log(inputCuisine ,cuisine)
+  console.log(inputCuisine, cuisine)
   rad = $('#radius').val();
   var queryURL = 'https://developers.zomato.com/api/v2.1/search?apikey=' + apiKey + '&count=' + count + '&lat=' + lat + '&lon=' + lon + '&cuisines=' + cuisine;
   console.log(queryURL);
-  
-  $.ajax({
-      url: queryURL,
-      method: 'GET'
-  }).then(function (response) {
-    $("#results").html("").attr("class","row center");
-      var rand = Math.floor(Math.random()*10);
-      var res = response.restaurants[rand].restaurant;
-      var newlat = res.location.latitude;
-      var newlon = res.location.longitude;
-      var resName = $("<h3>").text(res.name);
-      var resLink = $("<a>").attr("href", res.url);
-      $("#results").append(resLink);
-      resLink.append(resName);
-      var resThumb = $("<img>").attr("src",res.thumb);
-      $("#results").append(resThumb);
-      var resTimes = $("<p>").text(res.timings);
-      $("#results").append(resTimes);
-      var resRating = $("<p>").text("Average Rating from 0 to 5 is: " + res.user_rating.aggregate_rating);
-      $("#results").append(resRating);
 
-console.log(resName)
-callMap(newlat,newlon);
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function (response) {
+    $("#results").html("").attr("class", "row center");
+    var rand = Math.floor(Math.random() * 10);
+    var res = response.restaurants[rand].restaurant;
+    var newLat = res.location.latitude;
+    var newLon = res.location.longitude;
+    var resName = $("<h3>").text(res.name);
+    var resLink = $("<a>").attr("href", res.url);
+    $("#results").append(resLink);
+    resLink.append(resName);
+    var resThumb = $("<img>").attr("src", res.thumb);
+    $("#results").append(resThumb);
+    var resTimes = $("<p>").text(res.timings);
+    $("#results").append(resTimes);
+    var resRating = $("<p>").text("Average Rating from 0 to 5 is: " + res.user_rating.aggregate_rating);
+    $("#results").append(resRating);
+
+    console.log(resName)
+    callMap(newLat, newLon);
   });
 }
 
-function callMap(newlat,newlon) {
+function callMap(newLat, newLon) {
   $("#map").attr("style", "block")
-  var myLatLng=new google.maps.LatLng(newlat, newlon);
-  var marker = new google.maps.Marker({position: myLatLng, map: map});
+  var myLatLng = new google.maps.LatLng(newLat, newLon);
+  var marker = new google.maps.Marker({ position: myLatLng, map: map });
   map = new google.maps.Map(document.getElementById("map"));
-    map.setCenter(myLatLng);
-    map.setZoom(17);
-    marker.setMap(map);
-    console.log(newlat,newlon);
-  
+  map.setCenter(myLatLng);
+  map.setZoom(17);
+  marker.setMap(map);
+  console.log(newLat, newLon);
+
+var newSearch = $("<ul>");
+var newSearchBtn = $("<li>");
+newSearch.attr("id", "nav-mobile");
+newSearch.addClass("left");
+$("#navBar").append(newSearch);
+newSearchBtn.html('<a href="index.html">Search Again</a>');
+newSearch.append(newSearchBtn);
 }
